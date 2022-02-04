9ceb5498426a9484d08338d6703335255d462c27
giie30 <getse@parrotlabs.com.mx>
2022-02-04 10:45:52 -0600

    asd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import S3 from "react-aws-s3";
+import { useRef } from "react";
+
+const config = {
+  bucketName: "monitor-app-dev",
+  dirName: "photos" /* optional */,
+  region: "us-west-1",
+  accessKeyId: "AKIAT4JDCUNRGGBXZKJT",
+  secretAccessKey: "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"
+};
 
 function App() {
+  const fileInput = useRef();
+
+  const handleClick = e => {
+    e.preventDefault();
+    console.log(fileInput.current);
+    const file = fileInput.current.files[0];
+    const ReactS3Client = new S3(config);
+    ReactS3Client.uploadFile(file, file.name).then(data => {
+      console.log(data);
+      if (data.status === 204) {
+        console.log("success");
+      } else {
+        console.log("fail");
+      }
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleClick}>
+          <label>
+            Upload image
+            <input type="file" ref={fileInput} />
+          </label>
+          <br />
+          <button type="submit">Upload</button>
+        </form>
       </header>
     </div>
   );
